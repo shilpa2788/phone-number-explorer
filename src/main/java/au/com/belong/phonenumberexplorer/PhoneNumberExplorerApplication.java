5c2806926bc01149380a537fca2a7827e99f011c
shilpa2788 <44986191+shilpa2788@users.noreply.github.com>
2021-10-23 08:23:47 +1100

    feature branch commit

--- src/main/java/au/com/belong/phonenumberexplorer/PhoneNumberExplorerApplication.java
+++ src/main/java/au/com/belong/phonenumberexplorer/PhoneNumberExplorerApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhoneNumberExplorerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhoneNumberExplorerApplication.class, args);
-	}
 
+
+		public static void main(final String[] args) {
+
+			SpringApplication.run(PhoneNumberExplorerApplication.class, args);
+		}
 }
